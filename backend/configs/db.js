7ba15770c193f8330fce13e4403686a7f2cc3738
Barnik Podder <barnikpodder2003@gmail.com>
2025-03-22 15:18:14 +0530

    minor change

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -7,7 +7,7 @@ const mongodb = async () =>{
     try {
         await mongoose.connect(process.env.MONGO_DB_URL);
         console.log('MongoDB Connection Succeeded.');
-        await handleStartupTasks();
+        handleStartupTasks();
     }
     catch(error){
         console.log('Error in DB connection: ' + error);
